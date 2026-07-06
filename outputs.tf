7b015c63a78589010b4da32b96817ec0a6dfc6fc
Daniel Allen <dan@aeternagroup.net>
2026-07-06 23:13:31 +0100

    Update to schema version v4.80.0

--- outputs.tf
+++ outputs.tf
@@ -2,4 +2,16 @@ output "machine_learning_workspace_network_outbound_rule_fqdns" {
   description = "All machine_learning_workspace_network_outbound_rule_fqdn resources"
   value       = azurerm_machine_learning_workspace_network_outbound_rule_fqdn.machine_learning_workspace_network_outbound_rule_fqdns
 }
+output "machine_learning_workspace_network_outbound_rule_fqdns_destination_fqdn" {
+  description = "List of destination_fqdn values across all machine_learning_workspace_network_outbound_rule_fqdns"
+  value       = [for k, v in azurerm_machine_learning_workspace_network_outbound_rule_fqdn.machine_learning_workspace_network_outbound_rule_fqdns : v.destination_fqdn]
+}
+output "machine_learning_workspace_network_outbound_rule_fqdns_name" {
+  description = "List of name values across all machine_learning_workspace_network_outbound_rule_fqdns"
+  value       = [for k, v in azurerm_machine_learning_workspace_network_outbound_rule_fqdn.machine_learning_workspace_network_outbound_rule_fqdns : v.name]
+}
+output "machine_learning_workspace_network_outbound_rule_fqdns_workspace_id" {
+  description = "List of workspace_id values across all machine_learning_workspace_network_outbound_rule_fqdns"
+  value       = [for k, v in azurerm_machine_learning_workspace_network_outbound_rule_fqdn.machine_learning_workspace_network_outbound_rule_fqdns : v.workspace_id]
+}
 
