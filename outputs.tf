2372bee18d5c65b89a3645043636fd472d9bde42
Daniel Allen <dan@aeternagroup.net>
2026-07-12 16:03:10 +0100

    Update to schema version v4.80.0

--- outputs.tf
+++ outputs.tf
@@ -1,3 +1,7 @@
+output "machine_learning_workspace_network_outbound_rule_fqdns_id" {
+  description = "Map of id values across all machine_learning_workspace_network_outbound_rule_fqdns, keyed the same as var.machine_learning_workspace_network_outbound_rule_fqdns"
+  value       = { for k, v in azurerm_machine_learning_workspace_network_outbound_rule_fqdn.machine_learning_workspace_network_outbound_rule_fqdns : k => v.id }
+}
 output "machine_learning_workspace_network_outbound_rule_fqdns_destination_fqdn" {
   description = "Map of destination_fqdn values across all machine_learning_workspace_network_outbound_rule_fqdns, keyed the same as var.machine_learning_workspace_network_outbound_rule_fqdns"
   value       = { for k, v in azurerm_machine_learning_workspace_network_outbound_rule_fqdn.machine_learning_workspace_network_outbound_rule_fqdns : k => v.destination_fqdn }
